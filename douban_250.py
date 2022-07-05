6a26e7f6c9e800d33a822d020b803f51e1becc7f
zbz <zbz26678325@163.com>
2022-07-05 14:56:52 +0800

    update

--- douban_250.py
+++ douban_250.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 header = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
 }
